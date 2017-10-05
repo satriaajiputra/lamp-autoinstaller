4543917f51f0c0b648c31152627f7f20ce0752b5
Satria Aji Putra <satriamaxt@gmail.com>
2017-10-05 18:46:19 +0700

    Fix webmin

--- src/ubuntu/install.sh
+++ src/ubuntu/install.sh
@@ -88,7 +88,7 @@ if [ $rwebmin == "y" ]
 		echo "deb http://download.webmin.com/download/repository sarge contrib" > /etc/apt/sources.list.d/webmin.list
 		echo "deb http://webmin.mirror.somersettechsolutions.co.uk/repository sarge contrib
 		" >> /etc/apt/sources.list.d/webmin.list
-		wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add - -y
+		wget -q http://www.webmin.com/jcameron-key.asc -O- | sudo apt-key add -
 		apt-get update -y
 		apt-get install webmin -y
 
